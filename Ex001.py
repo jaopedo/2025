34d3c8f6ead1dadc72c4a7f2159dcf98142d5996
abacaxi com cenoura <151550223+jaopedo@users.noreply.github.com>
2025-03-07 09:18:14 -0300

    Add files via upload

---
+++ Ex001.py
@@ -0,0 +1,23 @@
+
+print ('Notas dos Semestres')
+
+print ('Primeiro Semestre')
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+n3 = float(input('Terceira nota: '))
+se1 = n1 + n2 + n3 
+
+print ('Soma do Primeiro Semestre: ' ,se1)
+
+
+
+print ('Segundo Semestre')
+
+n1 = float(input('Primeira nota: '))
+n2 = float(input('Segunda nota: '))
+n3 = float(input('Terceira nota: '))
+se2 = n1 + n2 + n3 
+
+print ('Soma do Segundo Semestre: '  ,se2)
+  
